2f6a5a5d6ab46345d29dcb8469b4060e87948f38
redhemd <rakhmetov_ardak@mail.ru>
2021-10-21 12:06:53 +0600

    added vue funtionals

--- vue-kolesa-upgrade/src/App.vue
+++ vue-kolesa-upgrade/src/App.vue
@@ -72,22 +72,165 @@
 </div>
 
 <div class="good-types body-main-block__good-types">
-<input type="radio" name="type" id="all-goods" checked>
+<input type="radio" name="type" id="all-goods"  value="0" v-model="picked" checked>
 <label data-key="all-goods" class="good-types__all js__good-types"
-for="all-goods" @click="showAllGoods">Все товары</label>
+for="all-goods" @click="clear">Все товары</label>
 
-<input type="radio" name="type" id="clothes">
+<input type="radio" name="type" id="clothes" value="1" v-model="picked">
 <label data-key="clothes" class="good-types__clothes js__good-types"
-for="clothes" @click="showClothes">Одежда</label>
+for="clothes" @click="clear">Одежда</label>
 
-<input type="radio" name="type" id="accesses">
+<input type="radio" name="type" id="accesses" value="2" v-model="picked">
 <label data-key="accesses" class="good-types__accesses js__good-types"
-for="accesses" @click="showAccesses">Аксессуары</label>
+for="accesses" @click="clear">Аксессуары</label>
 </div>
 
-<div class="card-products js__cards"></div>
+<div class="card-products js__cards">
+  <div class="sortedAllCards" v-for="(card, index) in sortedAllCards" :key="index">
+      <div :data-id="card.id" class="product card-products__product">
+        <img :src="card.img" alt="">
+        <span class="product__card-new" v-if="card.isNew">NEW</span>
+        <div class="product__card-info">
+          <span class="product__card-info__points">{{ card.price }} баллов</span>
+          <span class="product__card-info__descr">{{ card.title }}</span>
+          <span class="product__card-info__sizes">Размеры S/M/L</span>
+          <button class="product__card-info__btn" @click="openModal(card)">Заказать</button>
+        </div>
+      </div>
+  </div>
+</div>
 
-<div class="modal-main"></div>
+<div class="card-products" v-if="picked === '0'">
+  <div class="sortedAllCards" v-for="(card, index) in sortedAllCards" :key="index">
+      <div :data-id="card.id" class="product card-products__product">
+        <img :src="card.img" alt="">
+        <span class="product__card-new" v-if="card.isNew">NEW</span>
+        <div class="product__card-info">
+          <span class="product__card-info__points">{{ card.price }} баллов</span>
+          <span class="product__card-info__descr">{{ card.title }}</span>
+          <span class="product__card-info__sizes">Размеры S/M/L</span>
+          <button class="product__card-info__btn" @click="openModal(card)">Заказать</button>
+        </div>
+      </div>
+  </div>
+</div>
+
+<div class="card-products" v-if="picked === '1'">
+  <div class="sortedAllCards" v-for="(card, index) in clothes" :key="index">
+      <div :data-id="card.id" class="product card-products__product">
+        <img :src="card.img" alt="">
+        <span class="product__card-new" v-if="card.isNew">NEW</span>
+        <div class="product__card-info">
+          <span class="product__card-info__points">{{ card.price }} баллов</span>
+          <span class="product__card-info__descr">{{ card.title }}</span>
+          <span class="product__card-info__sizes">Размеры S/M/L</span>
+          <button class="product__card-info__btn" @click="openModal(card)">Заказать</button>
+        </div>
+      </div>
+  </div>
+</div>
+
+<div class="card-products" v-if="picked === '2'">
+  <div class="sortedAllCards" v-for="(card, index) in accesses" :key="index">
+      <div :data-id="card.id" class="product card-products__product">
+        <img :src="card.img" alt="">
+        <span class="product__card-new" v-if="card.isNew">NEW</span>
+        <div class="product__card-info">
+          <span class="product__card-info__points">{{ card.price }} баллов</span>
+          <span class="product__card-info__descr">{{ card.title }}</span>
+          <span class="product__card-info__sizes">Размеры S/M/L</span>
+          <button class="product__card-info__btn" @click="openModal(card)">Заказать</button>
+        </div>
+      </div>
+  </div>
+</div>
+
+<div class="modal-main">
+<div class="modal-bg">
+<div class="modal modal-bg__modal">
+<div class="pictures-info modal__pictures-info">
+<div class="pictures-info-primary pictures-info__pictures-info-primary">
+<img src='./assets/t-shirt-0.jpg' alt="Main Picture Of Jumper" />
+</div>
+<div class="pictures-info-secondary pictures-info__pictures-info-secondary">
+<div class="pictures-info-secondary__first">
+<img src='./assets/t-shirt-0.jpg' alt="Secondary Photo Of Jumper" />
+</div>
+<div class="pictures-info-secondary__second">
+<img src='./assets/t-shirt-0.jpg' alt="Secondary Photo Of Jumper" />
+</div >
+<div class="pictures-info-secondary__third">
+<img src='./assets/t-shirt-0.jpg' alt="Secondary Photo Of Jumper" />
+</div>
+</div>
+</div>
+
+<div class="main-info">
+<div class="main-info-header">
+<span class="main-info-header-text">Футболка "A Programmer's Life"</span>
+<span class="main-info-price-text">150 баллов</span>
+<button type="button" class="modal-btn">
+<span class="btn-text">Заказать</span>
+</button>
+</div>
+
+<div class="main-info-body">
+<div class="colors-block">
+<p class="colors-block-text">Цвета:</p>
+<div class="colors-toggle">
+<input class="blue-tog" type="radio" name="color" id="blue" checked>
+<label class="color colors-toggle-text blue-toggle-text colors-toggle__blue" for="blue">
+Синий
+</label>
+<input class="beige-tog" type="radio" name="color" id="beige">
+<label class="color colors-toggle-text blue-toggle-text colors-toggle__beige" for="beige">
+Бежевый
+</label>
+<input class="grey-tog" type="radio" name="color" id="grey">
+<label class="color colors-toggle-text blue-toggle-text colors-toggle__grey" for="grey">
+Серый
+</label>
+</div>
+</div>
+
+<div class="sizes-block">
+<p class="sizes-block-text">Размер:</p>
+<div class="sizes-toggle">
+<input type="radio" name="size" id="s" checked>
+<label class="size size-toggle-text blue-toggle-text sizes-toggle__S" for="s">S</label>
+<input type="radio" name="size" id="m" >
+<label class="size size-toggle-text blue-toggle-text sizes-toggle__M" for="m">M</label>
+<input type="radio" name="size" id="l" >
+<label class="size size-toggle-text blue-toggle-text sizes-toggle__L" for="l">L</label>
+</div>
+</div>
+
+<div class="footer-block">
+<div class="footer-block-details">
+<span class="footer-block-details-first">Детали:</span>
+<span class="footer-block-details-second">
+Брендированная толстовка от Qazaq Republic. Материал: Хлопок 80%, Вискоза 20%
+</span>
+</div>
+<div class="footer-block-additional-info">
+<span class="footer-block-add-first">Как выбрать размер:</span>
+<span class="footer-block-add-second">Написать дяде Рику для уточнения.</span>
+</div>
+</div>
+</div>
+</div>
+
+<div class="balance-info">
+<div class="balance-text">
+<span class="your-balance">Твой баланс:</span>
+<p class="your-balance-points">3 945 баллов</p>
+</div>
+<img class="balance-icon" src='./assets/cart-pic.jpg' alt="Balance Icon Picture"/>
+</div>
+<input type="image" src="./assets/modal-close-pic.jpg" class="modal-close" @click="closeModal">
+</div>
+</div>
+</div>
 </div>
 </div>
 
@@ -121,6 +264,8 @@ export default {
   name: 'App',
   data() {
     return {
+      picked: 0,
+      selectedTab: 0,
       /* eslint-disable global-require */
       clothes: [
         {
@@ -210,179 +355,24 @@ export default {
       /* eslint-enable global-require */
     };
   },
-  mounted() {
-    const arr = this.clothes.concat(this.accesses);
-    arr.sort((a, b) => b.isNew - a.isNew);
-    arr.forEach((card) => {
-      const {
-        img, isNew, price, title, id,
-      } = card;
-      const cardHTML = this.makeProductCard(img, isNew, price, title, id);
-      document.querySelector('.js__cards').innerHTML += cardHTML;
-    });
-    this.openModal();
-  },
-  methods: {
-    makeProductCard(img, isNew, price, title, id) {
-      return `<div data-id=${id} class="product card-products__product">
-                <img src=${img} alt="">
-                ${isNew ? '<span class="product__card-new">NEW</span>' : ''}
-                <div class="product__card-info">
-                    <span class="product__card-info__points">${price} баллов</span>
-                    <span class="product__card-info__descr">${title}</span>
-                    <span class="product__card-info__sizes">Размеры S/M/L</span>
-                    <button class="product__card-info__btn">Заказать</button>
-                </div>
-              </div>`;
-    },
-    makeModal(img, title, price, cartImg, closeImg) {
-      return `<div class="modal-bg">
-                <div class="modal modal-bg__modal">
-                    <div class="pictures-info modal__pictures-info">
-                    <div class="pictures-info-primary pictures-info__pictures-info-primary">
-                    <img src=${img} alt="Main Picture Of Jumper" />
-                    </div>
-                    <div class="pictures-info-secondary pictures-info__pictures-info-secondary">
-                    <div class="pictures-info-secondary__first">
-                    <img src="${img}" alt="Secondary Photo Of Jumper" />
-                    </div>
-                    <div class="pictures-info-secondary__second">
-                    <img src="${img}" alt="Secondary Photo Of Jumper" />
-                    </div >
-                    <div class="pictures-info-secondary__third">
-                    <img src="${img}" alt="Secondary Photo Of Jumper" />
-                    </div>
-                    </div>
-                    </div>
-
-                    <div class="main-info">
-                    <div class="main-info-header">
-                    <span class="main-info-header-text">${title}</span>
-                    <span class="main-info-price-text">${price} баллов</span>
-                    <button type="button" class="modal-btn">
-                    <span class="btn-text">Заказать</span>
-                    </button>
-                    </div>
-
-                    <div class="main-info-body">
-                    <div class="colors-block">
-                    <p class="colors-block-text">Цвета:</p>
-                    <div class="colors-toggle">
-                    <input class="blue-tog" type="radio" name="color" id="blue" checked>
-                    <label class="color colors-toggle-text blue-toggle-text colors-toggle__blue" for="blue">Синий</label>
-                    <input class="beige-tog" type="radio" name="color" id="beige">
-                    <label class="color colors-toggle-text blue-toggle-text colors-toggle__beige" for="beige">Бежевый</label>
-                    <input class="grey-tog" type="radio" name="color" id="grey">
-                    <label class="color colors-toggle-text blue-toggle-text colors-toggle__grey" for="grey">Серый</label>
-                    </div>
-                    </div>
-
-                    <div class="sizes-block">
-                    <p class="sizes-block-text">Размер:</p>
-                    <div class="sizes-toggle">
-                    <input type="radio" name="size" id="s" checked>
-                    <label class="size size-toggle-text blue-toggle-text sizes-toggle__S" for="s">S</label>
-                    <input type="radio" name="size" id="m" >
-                    <label class="size size-toggle-text blue-toggle-text sizes-toggle__M" for="m">M</label>
-                    <input type="radio" name="size" id="l" >
-                    <label class="size size-toggle-text blue-toggle-text sizes-toggle__L" for="l">L</label>
-                    </div>
-                    </div>
-
-                    <div class="footer-block">
-                    <div class="footer-block-details">
-                    <span class="footer-block-details-first">Детали:</span>
-                    <span class="footer-block-details-second">
-                    Брендированная толстовка от Qazaq Republic. Материал: Хлопок 80%, Вискоза 20%
-                    </span>
-                    </div>
-                    <div class="footer-block-additional-info">
-                    <span class="footer-block-add-first">Как выбрать размер:</span>
-                    <span class="footer-block-add-second">Написать дяде Рику для уточнения.</span>
-                    </div>
-                    </div>
-                    </div>
-                    </div>
-
-                    <div class="balance-info">
-                    <div class="balance-text">
-                    <span class="your-balance">Твой баланс:</span>
-                    <p class="your-balance-points">3 945 баллов</p>
-                    </div>
-                    <img class="balance-icon" src="${cartImg}" alt="Balance Icon Picture"/>
-                    </div>
-                    <input type="image" src="${closeImg}" class="modal-close">
-                </div>
-            </div>
-            `;
-    },
-    closeModal() {
-      document.querySelector('.modal-close').addEventListener('click', () => {
-        document.querySelector('.modal-bg').remove();
-      });
-    },
-    openModal() {
-      document.querySelectorAll('.product').forEach((product) => {
-        const arr = this.clothes.concat(this.accesses);
-
-        product.addEventListener('click', (event) => {
-          const productId = parseInt(product.dataset.id, 10);
-          let cardToggle = event.target;
-
-          cardToggle = productId;
-          const modalMain = document.querySelector('.modal-main');
-
-          arr.find(({
-            id, img, title, price, cartImg, closeImg,
-          }) => {
-            if (id === cardToggle) {
-              modalMain.innerHTML += this.makeModal(img, title, price, cartImg, closeImg);
-              return null;
-            }
-            return null;
-          });
-          this.closeModal();
-        });
-      });
-    },
-    showAllGoods() {
-      document.querySelector('.js__cards').innerHTML = '';
+  computed: {
+    sortedAllCards() {
       const arr = this.clothes.concat(this.accesses);
       arr.sort((a, b) => b.isNew - a.isNew);
-
-      arr.forEach((card) => {
-        const {
-          img, isNew, price, title, id,
-        } = card;
-        const cardHTML = this.makeProductCard(img, isNew, price, title, id);
-
-        document.querySelector('.js__cards').innerHTML += cardHTML;
-      });
-      this.openModal();
+      return arr;
     },
-    showClothes() {
-      document.querySelector('.js__cards').innerHTML = '';
-      this.clothes.forEach((card) => {
-        const {
-          img, isNew, price, title, id,
-        } = card;
-        const cardHTML = this.makeProductCard(img, isNew, price, title, id);
-
-        document.querySelector('.js__cards').innerHTML += cardHTML;
-      });
-      this.openModal();
+  },
+  methods: {
+    closeModal() {
+      const modalMain = document.querySelector('.modal-main');
+      modalMain.style.display = 'none';
     },
-    showAccesses() {
+    openModal() {
+      const modalMain = document.querySelector('.modal-main');
+      modalMain.style.display = 'block';
+    },
+    clear() {
       document.querySelector('.js__cards').innerHTML = '';
-      this.accesses.forEach((card) => {
-        const {
-          img, isNew, price, title, id,
-        } = card;
-        const cardHTML = this.makeProductCard(img, isNew, price, title, id);
-
-        document.querySelector('.js__cards').innerHTML += cardHTML;
-      });
-      this.openModal();
     },
   },
 };
