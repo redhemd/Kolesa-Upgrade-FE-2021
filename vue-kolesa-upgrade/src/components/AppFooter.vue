42ac66b4614abaaca8137227fb59bfee441ba96d
redhemd <rakhmetov_ardak@mail.ru>
2021-10-29 11:06:58 +0600

    added some code

--- vue-kolesa-upgrade/src/components/AppFooter.vue
+++ vue-kolesa-upgrade/src/components/AppFooter.vue
@@ -1,22 +1,20 @@
 <template>
-  <div class="footer container__footer">
-<div class="footer-info footer__footer-info">
+<div class="footer container__footer">
+  <div class="footer-info footer__footer-info">
+    <span>© Kolesa Group</span>
+    <div class="social-icons footer-info__social-icons">
+    <img class="icon-inst" src="../assets/social-instagram-icon.svg" alt="Social Instagram">
+    <img class="icon-youtube" src="../assets/social-youtube-icon.svg" alt="Social Youtube">
+    <img class="icon-vk" src="../assets/social-vk-icon.svg" alt="Social VK">
+    </div>
+  </div>
 
-<span>© Kolesa Group</span>
-<div class="social-icons footer-info__social-icons">
-<img class="icon-inst" src="../assets/social-instagram-icon.svg" alt="Social Instagram">
-<img class="icon-youtube" src="../assets/social-youtube-icon.svg" alt="Social Youtube">
-<img class="icon-vk" src="../assets/social-vk-icon.svg" alt="Social VK">
-</div>
+  <div class="suggestions footer__suggestions">
+    <span class="suggestions__text">Есть идеи что улучшить?</span>
+    <span>Не знаешь, с кем решить проблему?</span>
+  </div>
 
-</div>
-
-<div class="suggestions footer__suggestions">
-<span class="suggestions__text">Есть идеи что улучшить?</span>
-<span>Не знаешь, с кем решить проблему?</span>
-</div>
-
-<button class="footer-btn footer__footer-btn">Написать</button>
+  <button class="footer-btn footer__footer-btn">Написать</button>
 </div>
 </template>
 
